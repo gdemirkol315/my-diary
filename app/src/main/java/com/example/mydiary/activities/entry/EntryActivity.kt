00bbd62d4f1b3c6f978be1f3c9b73021ee3e1787
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-11-04 16:46:21 +0100

    icon for save added

--- app/src/main/java/com/example/mydiary/activities/entry/EntryActivity.kt
+++ app/src/main/java/com/example/mydiary/activities/entry/EntryActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -153,6 +154,10 @@ fun EntryScreen(onNavigateBack: () -> Unit, entry: Entry? = null) {
                             color = MaterialTheme.colorScheme.onPrimary
                         )
                     } else {
+                        Icon(
+                            imageVector = Icons.Default.Save,
+                            contentDescription = "Save"
+                        )
                         Text("Save")
                     }
                 }
