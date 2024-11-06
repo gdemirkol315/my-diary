5b8c1c1f10de43c87a1e1482dcd88187ac90385a
Demirkol, Goerkem <gdemirkol315@gmail.com>
2024-11-06 16:26:00 +0100

    DateUtils fix for handling parsing exception

--- app/src/main/java/com/example/mydiary/utils/DateUtils.kt
+++ app/src/main/java/com/example/mydiary/utils/DateUtils.kt
@@ -1,15 +1,21 @@
 package com.example.mydiary.utils
 
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 object DateUtils {
     fun reformatDateString(dateString: String): String {
+        if (dateString.isEmpty()) return ""
 
-        val inputFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
-        val date = inputFormat.parse(dateString) ?: return ""
-        val outputFormat = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
+        return try {
+            val inputFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH)
+            val date = inputFormat.parse(dateString) ?: return ""
+            val outputFormat = SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH)
 
-        return outputFormat.format(date)
+            outputFormat.format(date)
+        } catch (e: ParseException) {
+            ""
+        }
     }
 }
